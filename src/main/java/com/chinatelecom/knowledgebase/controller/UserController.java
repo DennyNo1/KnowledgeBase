4a3900556b79d2973d024a8c5874a25d6b068d5e
DennyNo1 <zht2041359@vip.qq.com>
2024-05-22 17:03:02 +0800

    修改了修改密码的接口

--- src/main/java/com/chinatelecom/knowledgebase/controller/UserController.java
+++ src/main/java/com/chinatelecom/knowledgebase/controller/UserController.java
@@ -67,7 +67,7 @@ public class UserController
 
             return R.success(response,"登录成功");
         }
-        else return R.error("请输入正确的密码");
+        else return R.error("您输入的密码有误，请重新输入");
         //return one;
     }
 
@@ -80,10 +80,65 @@ public class UserController
             User one = userImpl.getOne(queryWrapper);
             if(one==null)
                 return R.error("该用户名不存在，请重新输入");
-            else return R.success(one,"成功查询到该用户");
+            //应对特殊情况，没有设定过初始密码但选择忘记密码
+
+            else if(one.getSafeQuestion()==null){
+                return R.error("请先登录。如果遗忘默认密码，请联系慧问工作室徐慧15305809539。");
+            }
+            return R.success(one,"成功传输用户信息");
+
         }
         catch (Exception e){
             return R.error(e.getMessage());
+        }
+
+
+    }
+
+    //重置密码
+    @PostMapping("/reset")
+    public R reset(@RequestBody User user){
+        //先通过用户名查出用户
+        QueryWrapper<User> queryWrapper=new QueryWrapper<>();
+        queryWrapper.eq("username",user.getUsername());
+        //因为有了上一步check，正常来说是能查询到这个用户
+        User one = userImpl.getOne(queryWrapper);
+
+        //这个是用于初始化密码
+        if(one.getSafeQuestion()==null)
+        {
+            one.setPassword(user.getPassword());
+            one.setSafeQuestion(user.getSafeQuestion());
+            one.setSafeAnswer(user.getSafeAnswer());
+            try{
+                //修改的时候可能报错
+                userImpl.updateById(one);
+                return R.success(null,"初始化密码成功");
+
+            }
+            catch (Exception e){
+                return R.error(e.getMessage());
+            }
+        }
+
+        else{
+            //这个是用于重置密码
+            if(one.getSafeAnswer().equals(user.getSafeAnswer())&&one.getSafeQuestion().equals(user.getSafeQuestion()))
+
+
+            {
+                one.setPassword(user.getPassword());
+                try{
+                    //修改的时候可能报错
+                    userImpl.updateById(one);
+                    return R.success(null,"重置密码成功");
+
+                }
+                catch (Exception e){
+                    return R.error(e.getMessage());
+                }
+            }
+            else return R.error("安全问题的答案不符合，请重新输入");
         }
 
 
